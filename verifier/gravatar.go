de3bf87fddee7baa4a145ab84613cdbd2b1e213a
Helielson <helielson@hotmail.com>
2017-10-19 01:08:15 -0300

    Always close response body in the HasGravatar function (#6)
    * Improve gravatar code
    
    * Fix logic

--- verifier/gravatar.go
+++ verifier/gravatar.go
@@ -6,9 +6,10 @@ import "net/http"
 // associated with a gravatar account
 func HasGravatar(a *Address) bool {
 	resp, err := http.Head("https://en.gravatar.com/" + a.MD5() + ".json")
-	if err != nil || resp.StatusCode != 200 {
+	if err != nil {
 		return false
 	}
+
 	defer resp.Body.Close()
-	return true
+	return resp.StatusCode == http.StatusOK
 }
