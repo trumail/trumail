a210c8e99985a8ceb3a14aab1861dc6d8a9b53b3
Steven Wolfe <steven@swolfe.me>
2018-04-07 14:11:39 -0700

    Not using html for stat state

--- web/assets/js/trumail.js
+++ web/assets/js/trumail.js
@@ -1,14 +1,17 @@
 $('.ui.dropdown').dropdown();
 
+var oldStats = {daily:{},monthly:{}};
+
 function pollStats() {
-    $.getJSON('/stats', function (data) {
+    $.getJSON('/stats', function (newStats) {
         // Set all countups
-        countup('dayDeliverable', data.daily.deliverable);
-        countup('dayUndeliverable', data.daily.undeliverable);
-        countup('daySuccessRate', data.daily.successRate, '', '%');
-        countup('monthDeliverable', data.monthly.deliverable);
-        countup('monthUndeliverable', data.monthly.undeliverable);
-        countup('monthSuccessRate', data.monthly.successRate, '', '%');
+        countup('dayDeliverable', oldStats.daily.deliverable, newStats.daily.deliverable);
+        countup('dayUndeliverable', oldStats.daily.undeliverable, newStats.daily.undeliverable);
+        countup('daySuccessRate', oldStats.daily.successRate, newStats.daily.successRate, '', '%');
+        countup('monthDeliverable', oldStats.monthly.deliverable, newStats.monthly.deliverable);
+        countup('monthUndeliverable', oldStats.monthly.undeliverable, newStats.monthly.undeliverable);
+        countup('monthSuccessRate', oldStats.monthly.successRate, newStats.monthly.successRate, '', '%');
+        oldStats = newStats; // Update the oldstats
 
         // Perform this action every 10 seconds
         setTimeout(pollStats, 10000); // Poll stats every 10 seconds and re-apply to UI
@@ -16,12 +19,10 @@ function pollStats() {
 }
 
 // countup animates the passed id with a new counted up value
-function countup(id, to, prefix, suffix) {
-    var from = $('#' + id).text(); // Retrieve the starting value
-    from = from.trim(); // Trim any whitespace
-    from = from.replace(/,/g, ''); // Remove all commas
-    if (from == '' || from > to) {
-        from = to;
+function countup(id, from, to, prefix, suffix) {
+    // if from isn't set yet, set it to the initial to value
+    if (from == undefined) {
+        from = to
     }
 
     // Configure countup options
